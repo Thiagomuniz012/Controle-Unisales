eab56d55edb4613ec307448bd913e90bd21f295f
Thiagomuniz012 <thiago.muniz012@gmail.com>
2023-04-17 21:25:29 -0300

    Atualizacao tela de login

--- js/index.js
+++ js/index.js
@@ -9,15 +9,15 @@ document.getElementById("login-button").addEventListener("click", function(event
     var userData = JSON.parse(storedData);
   
     // Definir usuário e senha padrão
-    var defaultUsername = "coordenador@salesiano.br"; //"coordenador@salesiano.br";
+    var defaultEmail = "coordenador@salesiano.br"; //"coordenador@salesiano.br";
     var defaultPassword = "coordenador2023"; //"coordenador2023";
   
     // Obter os dados do formulário
-    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
   
     // Verificar se os dados correspondem
-    if ((userData && userData.username === username && userData.password === password) || (username === defaultUsername && password === defaultPassword)) {
+    if ((userData && userData.email === email && userData.password === password) || (email === defaultEmail && password === defaultPassword)) {
       // Login bem-sucedido
       window.location.href = "Menu.html";
     } else {
@@ -29,12 +29,12 @@ document.getElementById("login-button").addEventListener("click", function(event
   
   function createAccount() {
     // Obter os dados do formulário
-    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
   
     // Criar o objeto de usuário
     var userData = {
-      username: username,
+      email: email,
       password: password
     };
   
@@ -42,3 +42,78 @@ document.getElementById("login-button").addEventListener("click", function(event
     localStorage.setItem("userData", JSON.stringify(userData));
   }
   
+
+  //Validacao do login
+  const form = document.getElementById('form')
+  const campos = document.querySelectorAll('.required')
+  const spans = document.querySelectorAll('.span-required')
+  const emailRegex = /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/;
+  
+  //Funcao para iniciar o erro
+  function setError(index){
+    campos[index].style.borderBottom = '2px solid #D91828'
+    spans[index].style.display = 'block'
+  }
+  
+  //Funcao para remover o erro
+  function removeError(index){
+    campos[index].style.borderBottom = ''
+    spans[index].style.display = 'none'
+  }
+  
+  //Valida o email
+  function emailValidate(){
+    if(!emailRegex.test(campos[0].value)){
+      setError(0)
+    } else {
+      removeError(0)
+    }
+  }
+  
+  //Valida a senha
+  function passwordValidate(){
+    if(campos[1].value === ''){
+      removeError(1)
+    } else if(campos[1].value.length < 8){
+      setError(1)
+    } else {
+      removeError(1)
+    }
+  }
+  
+  //Quando apagar todo o texto volta para o estado de origem
+  const emailInput = document.getElementById('email');
+  
+  emailInput.addEventListener('input', function(event) {
+    if (!event.target.value) {
+      event.target.style.borderBottom = '2px solid #2B448C';
+      const errorMessage = event.target.parentElement.querySelector('.span-required');
+      errorMessage.style.display = 'none';
+    }
+  });
+  
+  const passwordInput = document.getElementById('password');
+
+  passwordInput.addEventListener('input', function(event) {
+    if (!event.target.value) {
+      event.target.style.borderBottom = '2px solid #2B448C';
+      const errorMessage = event.target.parentElement.querySelector('.span-required');
+      errorMessage.style.display = 'none';
+    } else {
+      passwordValidate();
+    }
+  });
+
+//Funcao do botao de mostrar a senha
+  const showpasswordInput = document.getElementById("password");
+  const showPasswordButton = document.getElementById("show-password");
+
+  showPasswordButton.addEventListener("click", function() {
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text"
+    showPasswordButton.textContent
+  } else {
+    passwordInput.type = "password"
+    showPasswordButton.textContent
+  }
+});
